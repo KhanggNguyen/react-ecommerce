d1059eaf1687ce4474a23ba3197b757ad1c24257
Khang <kukhang2411@gmail.com>
2022-05-17 17:41:35 +0200

    fix link redirect problem

--- src/components/Breadcrumb/Breadcrumb.jsx
+++ src/components/Breadcrumb/Breadcrumb.jsx
@@ -1,12 +1,16 @@
 import { Breadcrumbs, Typography } from "@material-ui/core";
 import React from "react";
-import { Link } from "@material-ui/core";
+//import { Link } from "@material-ui/core";
+import { Link } from 'react-router-dom';
+import useStyles from "./styles";
 
 const Breadcrumb = ({ breadcrumbs, currentPage }) => {
+    const classes = useStyles();
+
     return (
         <Breadcrumbs separator="›" aria-label="breadcrumb">
             {breadcrumbs?.map((breadcrumb, index) => (
-                <Link key={index} href={breadcrumb.path} >{breadcrumb.name}</Link>
+                <Link className={classes.a} key={index} to={breadcrumb.path} >{breadcrumb.name}</Link>
             ))}
             <Typography color='textPrimary'>{currentPage}</Typography>
         </Breadcrumbs>
